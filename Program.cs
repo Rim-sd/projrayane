41ab27e4ef0f68c7c9c83e78b699bd20a05112e6
Rim-sd <120680813+Rim-sd@users.noreply.github.com>
2026-07-22 14:43:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using appTest.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//le prob de bases de données
+builder.Services.AddDbContext<appTestDB>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}")
+    .WithStaticAssets();
+app.Run();
